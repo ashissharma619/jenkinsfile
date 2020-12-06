ae3f6ce3d50e9377df3ce9f45480c558e89845b9
ashis.sharma <ashis11.sharma@nttdata.com>
2020-12-06 21:23:28 +0000

    adding executable bit to pally-tests.sh file

---
+++ aem-cd/stages/pa11y-tests/common/pa11y-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# if [ ! -s "./ci/pa11y-config.json" ]; then 
+#    echo "The file ./ci/pa11y-config.json either doesn't exist or is empty."
+#    exit 1
+# fi
+
+echo -e "\n Pa11y testing started... \n"
+
+EXIT_CODE=0
+
+pa11y-ci --config ./ci/pa11y-config.json --json > pa11y-ci-results.json || EXIT_CODE=$?
+
+set -e
+
+pa11y-ci-reporter-html --source ./pa11y-ci-results.json --destination ./pa11y-ci-results
+
+rm pa11y-ci-results.json
+
+echo -e "\n Pa11y testing completed... \n"
+
+exit $EXIT_CODE
